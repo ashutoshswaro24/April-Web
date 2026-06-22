17cadac9fd8230a291ebd7427ada7f094aef4b09
ashutoshswaro24 <ashutoshswaro34@gmail.com>
2026-06-23 02:42:00 +0530

    react

---
+++ React/my-first-react/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Btn from "./button";
+function App(){
+
+return(
+ //HTML Code-
+<div>
+<h2>hello guys</h2>
+<button>login</button>
+
+<label>Username</label>
+<input  />
+<Btn    />
+
+
+</div>
+
+
+
+
+
+)
+
+
+}
+
+export default App;
---
+++ React/my-first-react/src/button.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Btn(){
+
+return(
+
+    <div>
+        <button style= {{ backgroundColor:'yellow' , padding:'10px' }}></button>
+    </div>
+)
+
+
+}
+
+
+
+export default Btn
